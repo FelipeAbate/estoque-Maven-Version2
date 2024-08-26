cde880ad5ff2f7cbaae2856344c953c08733f261
Felipe Abate <felipeabate2017@gmail.com>
2024-08-25 23:50:03 -0300

    push

--- estoqueRealese2-eclipse/src/main/java/abatesolutions/stockdesktop/com/Estoque2024/App.java
+++ estoqueRealese2-eclipse/src/main/java/abatesolutions/stockdesktop/com/Estoque2024/App.java
@@ -3,17 +3,18 @@ package abatesolutions.stockdesktop.com.Estoque2024;
 import java.awt.EventQueue;
 
 public class App {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            try {
+                LoginScreen loginScreen = new LoginScreen();
+                loginScreen.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
 
-	public static void main(String[] args) {
-		
-		InsertSQL insertInCamisetas = new InsertSQL();
-        insertInCamisetas.insert();
-        
-        UpdateSQL updateShirts = new UpdateSQL();
-        updateShirts.update("Nike", 67);
-        
-        DeleteSQL deleteInshirts = new DeleteSQL();
-        deleteInshirts.delete_sql(76);
+    //DeleteSQL deletePants = new DeleteSQL();
+    //deletePants.deleteInPants(18);   
         
 	}
 }
